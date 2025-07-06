4564d63a240924f3ef65f5b5965849e27f41f67f
digirabbithome <swimming8250@yahoo.com.tw>
2025-07-06 21:39:03 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.getElementById('generate').addEventListener('click', () => {
+  alert("✅ 小兔幫你產出貼文囉～\n\n🟢 Line ID：@digirabbit\n☎️ 02-27592006  02-27592013\n🏨 台北市信義區大道路74巷1號（捷運後山埤2號出口）");
+});
